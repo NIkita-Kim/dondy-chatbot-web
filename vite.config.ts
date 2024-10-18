1b1ccddcef99e18f7f776e3a2a0e8d38dac6d8ec
Nikita Kim <n.kim@softteco.com>
2024-10-18 18:00:42 +0300

    removed unused plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
   cacheDir: '../../../node_modules/.vite/frontend-sso',
@@ -16,7 +15,6 @@ export default defineConfig({
   },
 
   plugins: [
-    TanStackRouterVite(),
     react(),
   ],
 
